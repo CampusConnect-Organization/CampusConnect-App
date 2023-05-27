e31b70b3974f73cb5bb0da71b6f8a7ac8a34dbe7
Nishant Sapkota <snishant306@gmail.com>
2023-05-27 13:21:34 +0545

    feat: make redirection from splash to profile possible if profile exists

--- lib/view/splash.view.dart
+++ lib/view/splash.view.dart
@@ -1,18 +1,40 @@
 import 'dart:async';
 
+import 'package:campus_connect_app/models/profile.model.dart';
+import 'package:campus_connect_app/services/profile.service.dart';
 import 'package:campus_connect_app/utils/global.colors.dart';
+import 'package:campus_connect_app/view/home.view.dart';
 import 'package:campus_connect_app/view/login.view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<dynamic> checkAccessToken() async {
+    var data = await ProfileAPIService().getProfile();
+    if (data is Profile) {
+      Get.off(() => const HomeView());
+    } else {
       Get.off(() => const LoginView());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () async {
+      await checkAccessToken();
     });
     return Center(
       child: Scaffold(
